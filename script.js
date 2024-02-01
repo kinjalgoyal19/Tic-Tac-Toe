3f1ec8522097984feb17433d9fc61870fc89539e
Kinjal Goyal <111036667+kinjalgoyal19@users.noreply.github.com>
2024-02-01 23:37:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+let boxes = document.querySelectorAll(".box");
+let rstBtn = document.querySelector("#resetBtn");
+let newBtn = document.querySelector('#newBtn');
+let cont = document.querySelector(".msg");
+let p = document.querySelector('#msg');
+let turnX = true;
+let counter = 0;
+// array of array 
+const winPatters = [
+    [0,1,2], [0,3,6], [0,4,8],
+    [1,4,7], [2,5,8], [2,4,6],
+    [3,4,5], [6,7,8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener('click', () =>{
+        // console.log("clicked");
+        if(turnX){
+            box.innerText = 'X';
+            box.style.color = "red";
+            turnX = false;
+            
+        } else{
+            box.innerText = 'O';
+            box.style.color = "blue";
+            turnX = true;
+        }
+        box.disabled = true;
+        counter++;
+        checkWinner();
+        checkDraw();
+    })
+});
+
+const disabBox = () => {
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+
+const enabBox = () => {
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
+const rstGame =() =>{
+    turnX=true;
+    enabBox();
+    cont.classList.add("hide");
+}
+
+const showWinner = (winner) => {
+    p.innerText = `Winner is ${winner}`;
+    cont.classList.remove("hide");
+    disabBox();
+}
+
+const checkDraw = () => {
+    if(counter === boxes.length){
+        p.innerText = "It's a draw."
+        cont.classList.remove("hide");
+        disabBox();
+    }
+}
+
+const checkWinner = ()=> {
+    for(let pattern of winPatters){
+        // console.log(
+        //     boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText, 
+        //     boxes[pattern[2]].innerText
+        // );
+        let posVal = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(posVal!="" && pos2Val!="" && pos3Val!=""){
+            if(posVal===pos2Val && pos2Val===pos3Val){
+                // console.log("winner", posVal);
+                showWinner(posVal);
+            }
+        } 
+    }
+};
+
+newBtn.addEventListener("click", rstGame);
+rstBtn.addEventListener("click", rstGame);
